f522a1262fca8af1b7b52ba0a9daddea0a67b780
kevinferg <kevinferg98@gmail.com>
2023-02-08 11:15:11 -0500

    Format sampling plot better and close figure

--- examples/sampling_example.py
+++ examples/sampling_example.py
@@ -15,7 +15,8 @@ ubs = [ 1.0,  1.0]
 
 xy = rejection_sample(pdf, N, lbs, ubs, pdf_max = maxval)
 
-plt.figure(figsize=(6,6))
-plt.scatter(xy[:,0], xy[:,1], c="black", s=0.5)
+plt.figure(figsize=(4,4), dpi=100)
+plt.scatter(xy[:,0], xy[:,1], c="black", s=1)
 plt.axis("equal")
 plt.savefig("gaussian_2d_samples.png", bbox_inches="tight")
+plt.close()
